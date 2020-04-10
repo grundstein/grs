02168d3a4f4b184bd50e936a5f0faff09130ab5f
jascha <jascha@jaeh.at>
2020-04-10 08:33:40 +0200

    test/spec: add handler

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,9 +1,15 @@
 import { is } from '@magic/test'
 
-import defaultRunCluster, { run } from '../src/index.mjs'
+import defaultRun, { run } from '../src/index.mjs'
+
+import defaultHandler, { handler} from '../src/handler.mjs'
 
 export default [
-  { fn: () => run, expect: is.fn, info: 'runCluster is a function' },
-  { fn: () => defaultRunCluster, expect: is.fn, info: 'runCluster default export is a function' },
-  { fn: is.deep.eq(run, defaultRunCluster), info: 'runCluster exports are equal' },
+  { fn: () => run, expect: is.fn, info: 'run is a function' },
+  { fn: () => defaultRun, expect: is.fn, info: 'run default export is a function' },
+  { fn: is.deep.eq(run, defaultRun), info: 'run exports are equal' },
+
+  { fn: () => handler, expect: is.fn, info: 'handler is a function' },
+  { fn: () => defaultHandler, expect: is.fn, info: 'handler default export is a function' },
+  { fn: is.deep.eq(handler, defaultHandler), info: 'handler exports are equal' },
 ]
