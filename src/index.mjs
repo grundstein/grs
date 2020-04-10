33cf2808d9a13d02873b75c7dee9e467cc08a93c
jascha <jascha@jaeh.at>
2020-04-10 10:35:19 +0200

    format

--- src/index.mjs
+++ src/index.mjs
@@ -4,7 +4,7 @@ import { log, middleware } from '@grundstein/commons'
 
 import handler from './handler.mjs'
 
-export const run = async (args) => {
+export const run = async args => {
   const startTime = log.hrtime()
 
   const { port = 5321, host = '127.0.0.1', dir = 'public' } = args
@@ -13,11 +13,9 @@ export const run = async (args) => {
     const server = http.createServer(handler)
 
     const clientError = middleware.clientError({ host, port, startTime })
-
     server.on('clientError', clientError)
 
     const listener = middleware.listener({ host, port, startTime })
-
     server.listen(port, host, listener)
   } catch (e) {
     log.error(e)
